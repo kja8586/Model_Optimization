a898b4868899d79b65ea7f3fc71e55c5f72f3f23
kja8586 <102473609+kja8586@users.noreply.github.com>
2026-02-24 11:44:02 +0530

    Implement unstructured pruning function for models

---
+++ src/models/unstructured.py
@@ -0,0 +1,25 @@
+def apply_unstructured_pruning(model, final_sparsity, steps, epochs, lr):
+  pruning_params = {
+      'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(
+          initial_sparsity=0.0,
+          final_sparsity=final_sparsity,
+          begin_step=0,
+          end_step=steps*epochs
+      )
+  }
+  
+  unstructured_pruned = tfmot.sparsity.keras.prune_low_magnitude(
+      model, **pruning_params
+  )
+  unstructured_pruned.compile(
+      optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
+      loss='categorical_crossentropy',
+      metrics=['accuracy']
+  )
+  
+  callbacks = [
+      tfmot.sparsity.keras.UpdatePruningStep(),
+      tf.keras.callbacks.EarlyStopping(patience=3, restore_best_weights=True)
+  ]
+  
+  return unstructured_pruned, callbacks
